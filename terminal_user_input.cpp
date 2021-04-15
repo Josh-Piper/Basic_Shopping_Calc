d6ad0f59c85c66222991928e566ee8120c390695
PandaPlaysAll <josh.piper@live.com.au>
2021-04-15 10:38:42 +1000

    Adding Repository

---
+++ terminal_user_input.cpp
@@ -0,0 +1,108 @@
+#ifndef TERMINAL_USER_INPUT
+#define TERMINAL_USER_INPUT
+
+#include "splashkit.h"
+#include <string>
+
+using std::to_string;
+
+string read_string(string prompt) 
+{
+    write(prompt);
+    return read_line();
+}
+
+int read_integer(string prompt) 
+{
+
+    string result;
+    do
+    {
+        result = read_string(prompt);
+        if ( not is_integer (result) ) write_line("[INFO] Please enter a whole number");
+    } while ( not is_integer (result) );
+
+    return convert_to_integer(result);
+}
+
+int read_integer (string prompt, int min, int max) 
+{
+    int result { read_integer( prompt ) };
+
+    while ( result < min || result > max ) {
+        write_line("[INFO] Please enter a number between " + to_string(min) + " and " + to_string(max));
+        result = read_integer(prompt);
+    }
+
+    return result;
+}
+
+double read_double(string prompt) 
+{  
+    string result;
+
+    do 
+    {
+        result = read_string(prompt);
+        if  ( not is_double(result) ) write_line("[INFO] Please enter a decimal number");
+    } while ( not is_double(result) );
+
+    return convert_to_double(result);
+}
+
+double read_double_range(string prompt, double min, double max) 
+{
+    double result { read_double(prompt) };
+
+    while ( result < min || result > max ) {
+        write_line("The number must be between " + to_string(min).substr(0, 3) + " and " + to_string(max).substr(0, 3));
+        result = read_double(prompt);
+    }
+
+    return result;
+}
+
+int read_integer_with_length(string prompt, int length)
+{
+    int result;
+    int size_of_string;
+
+    do
+    {
+        result = read_integer(prompt);
+        size_of_string = to_string(result).length() + 1;
+        if ( size_of_string != length ) write_line("The amount of digits must be " + to_string(length));
+    } while ( size_of_string != length );
+
+    return result;
+}
+
+bool read_boolean(string prompt) 
+{
+    string line;
+    bool result;
+
+    do {
+        line = to_lowercase(trim(read_string(prompt)));
+
+        if (line == "y" || line == "yes") 
+        {
+            result = true;
+            break;
+        }
+        else if (line == "n" || line == "no") 
+        {
+            result = false;
+            break;
+        } else 
+        {
+            write_line("Please only enter y/yes or n/no");
+        }
+        
+    } while ( line != "n" || line != "no" || line != "y" || line != "yes");
+
+    return result;
+
+}
+
+ #endif
---
+++ terminal_user_input.h
@@ -0,0 +1,78 @@
+#include <string>
+
+using std::string;
+
+/**
+ * Prompts the user for input and reads the text that the user enters
+ * at the Terminal. This will read in a line of text and return this
+ * as a string
+ * 
+ * @param prompt    the message display to the user
+ * @returns         the text the user enters
+ */ 
+string read_string ( string prompt );
+
+
+/**
+ * Prompts the user for input and reads the text that the user enters
+ * at the Terminal. This will read in a line of text and return this
+ * as an integer if it is a valid integer
+ * 
+ * @param prompt    the message display to the user
+ * @returns         the number the user enters
+ */ 
+int read_integer ( string prompt );
+
+/**
+ * Prompts the user for input and reads the text that the user enters
+ * at the Terminal. This will read in a line of text and return this
+ * as an double if it is a valid double
+ * 
+ * @param prompt    the message display to the user
+ * @returns         the double the user enters
+ */ 
+double read_double(string prompt);
+
+/**
+ * Prompts the user for input and reads the text that the user enters
+ * at the Terminal. This will read in a line of text and return this
+ * as an integer. Note, the value must be between the minimum and maximum
+ * 
+ * @param prompt    the message display to the user
+ * @param min       minimum number
+ * @param max       maximum number
+ * @returns         the integer the user enters between min and max
+ */ 
+int read_integer (string prompt, int min, int max);
+
+/**
+ * Prompts the user for input and reads the text that the user enters
+ * at the Terminal. This will read in a line of text and return this
+ * as an double if the double is between min and max
+ * 
+ * @param prompt    the message display to the user
+ * @param min       minimum value the user can enter
+ * @param max       the maximum value the user can enter
+ * @returns         the double the user enters
+ */ 
+double read_double_range(string prompt, double min, double max);
+
+
+/**
+ * Prompts the user for input and reads the text that the user enters
+ * at the Terminal. This will read in a line of text and return this
+ * as an boolean
+ * 
+ * @param prompt    the message display to the user
+ * @returns         the boolean the user enters dependent on y/n yes/no
+ */ 
+bool read_boolean(string prompt);
+
+
+
+/**
+ * --TO DO--
+ * Enter information here
+ * 
+ */ 
+int read_integer_with_length(string prompt, int length);
